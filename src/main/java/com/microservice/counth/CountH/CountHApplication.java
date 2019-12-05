7fa7d0baa76ac2985b4734d9cf84c9c15189a55b
Davis Jehnson Verastegui Vitate <dverastv@everis.com>
2019-12-05 10:46:38 -0500

    version 1

---
+++ src/main/java/com/microservice/counth/CountH/CountHApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.counth.CountH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountHApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountHApplication.class, args);
+	}
+
+}
